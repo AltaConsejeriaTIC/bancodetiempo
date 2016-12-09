cba25a0caf8eaf76c24808e350696898e3818520
akod55 <akod55@gmail.com>
2016-12-09 10:48:35 -0500

    Titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
         <style>
             html, body {
                 background-color: #fff;
-                color: #636b6f;
+                color: red;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
                 height: 100vh;
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Banco de Tiempo
                 </div>
 
                 <div class="links">
