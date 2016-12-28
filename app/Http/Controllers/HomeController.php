6a0d035dc1d58bac5902ba23f7c6962cb01c8a6a
Cristian Daza <ccdaza2@gmail.com>
2016-12-28 12:09:42 -0500

    28/12/2016

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Service;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,10 @@ class HomeController extends Controller
      */
     public function index()
     {
+    	$allSevices = Service::where("user_id" , "!=", Auth::user()->id);
+    	
+    	dd($allSevices);
+    	
         return view('home');
     }
 
