b560a0ea81087c7c5128a05008c88e6a7fe5fad4
akod55 <akod55@gmail.com>
2016-12-29 15:21:03 -0500

    partial ImageProfile

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -5,18 +5,31 @@
 @include('nav',array('type' => 2))
 <div class="container">
     <div class="row">
-        <div class="col-md-3 col-xs-1">
+        <div class="col-md-4 col-xs-1">
           @include('partial.imageProfile')
-            <div class="userName">{{$user->first_name." ".$user->last_name}}</div>
+            <div class='ranking rankingProfile'>
+
+        			@for($cont = 1 ; $cont <= 5 ; $cont++)
+
+        				@if($cont <= $user->ranking)
+        					<span class='material-icons parrafo1'>grade</span>
+        				@else
+        					<span class='material-icons parrafo6 star'>fiber_manual_record</span>
+        				@endif
+
+        			@endfor
+
+        		</div>
+            <div class="userName titulo1">{{$user->first_name." ".$user->last_name}}</div>
             <div class="aboutMe">{{$user->aboutMe}}</div>
             <a href="{{ url('profile/edit') }}" class="btn btn-raised btn-primary col-md-12">   Editar</a>
             <a href="{{ url('#') }}" class="btn btn-raised btn-plus col-md-12">   Desactivar Cuenta</a>
 	            </div>
-	    	</div>
-        	</div>
-        <div class="col-md-6 col-xs-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">
+
+
+        <div class="col-md-8 col-xs-1">
+            <div class="">
+                <div class="">
                   <div class="row">
                     <p class="col-md-6">
                     Mis Servicios
@@ -24,40 +37,19 @@
                   <div class="col-md-6">
                       <a class="btn btn-raised btn-success" href="{{ url('/service') }}">Nuevo servicio</a>
                   </div>
-
                   </div>
+
                 </div>
                 @include('partial.errors')
-                <div class="panel-body ">
+                <div class="row">
                 @foreach($user->services as $key=>$service)
-
-                    <div class="col-md-6">
-                      <div class="photoService">
-                          <image  src="{{$service->image}}" />
-                      </div>
-                      <div class="contentService">
-                        <div class="titleService">
-                        <p>
-                            <a href='{{url("/service",["serviceid" => $service->id]) }}'> {{$service->name}}</a>
-                        </p>
-                        </div>
-                        <div class="descriptioService">
-                          <p>
-                            {{$service->description}}
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                    @if($key%2!=0)
-                        <div class="clearfix">
-
-                        </div>
-                    @endif
+                <div class="col-md-6 col-xs-1">
+                    @include('partial.serviceBox', array('service'=>$service))
+                </div>
                   @endforeach
               </div>
         </div>
 		</div>
+    </div>
 	</div>
-
-</div>
 @endsection
