51ac7047eaf8553e4e4735d724c74337fd459f2a
Cristian Daza <ccdaza2@gmail.com>
2016-12-29 14:54:17 -0500

    List services and partial serviceBox

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,30 +3,29 @@
 @section('content')
 @include('nav',array('type' => 2))
 
-<div class="panel">
-      <div class="container">
-          <ul  class="nav nav-pills">
-			<li class="active">
-        		<a  href="#1a" data-toggle="tab">Todos</a>
-			</li>
-		</ul>
+<div class="container">
+    <ul  class="nav nav-pills">
+		<li class="active">
+        	<a  href="#1a" data-toggle="tab">Todos</a>
+		</li>
+	</ul>
 
-		<div class="tab-content clearfix">
-			<div class="tab-pane active" id="1a">
-				<div class='row'>
-		          @foreach($allServices as $service)
+	<div class="tab-content clearfix">
+		<div class="tab-pane active" id="1a">
+			<div class='row'>
+		        @foreach($allServices as $key => $service)
 		          
-		          	<div class='col-md-4'>
+		          <div class='col-md-4 col-xs-12'>
 			          	
-			          	@include('partial/serviceBox', array("service" => $service))
+			          @include('partial/serviceBox', array("service" => $service))
 			          		          	
-		          	</div>
+		          </div>
 		          
-		          @endforeach
-		        </div>
-			</div>
-  		</div>
-    </div>
+		        @endforeach
+		    </div>
+		</div>
+  	</div>
 </div>
+
 
 @endsection
