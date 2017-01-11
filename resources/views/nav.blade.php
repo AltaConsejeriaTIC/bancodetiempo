3f6ed2ded678f771f93ad582c993119afa7094dc
Cristian Daza <ccdaza2@gmail.com>
2017-01-11 10:35:19 -0500

    Correccion bug commit git archivo nav.blade

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -4,20 +4,7 @@
 
 		<div class="container">
 			<div class='row'>
-<<<<<<< HEAD
-				<div class="col-md-6">
-					<a href="/" >
-						<i class="material-icons icon-logo">fiber_manual_record</i>
-							<h1 id="logo-container">BANCO DE TIEMPO</h1>
-							<h1 id="logo-container1">DE BOGOTÁ</h1>
-					</a>
-				</div>
-				@if((Auth::guest()))
-	        <div class="col-md-6" id="container-nav-buttons">
-	        	<button id="show-modal" @click="showModal = true" class="button5">{{ trans('dictionary.login') }}</button>
-	        	<button id="show-modal" @click="showModal = true" class="button4">Registrarse</button>
-	        </div>
-=======
+
 				<div class="col-xs-7 col-sm-3 col-md-3">					
 					<a href="/" >
 						<img src="images/logo.png" alt="Logo" />
@@ -28,7 +15,7 @@
 			        	<button id="show-modal" @click="showModal = true" class="button5">{{ trans('dictionary.login') }}</button>          	
 			        	<button id="show-modal" @click="showModal = true" class="button4 hidden-xs">Registrarse</button>          		          
 			        </div>
->>>>>>> f0bbabaa68cf1e20101b83cb60c6ab4184fc0f50
+
 				@elseif((!Auth::guest()))
 					<a class="col-xs-5 col-sm-4 col-sm-offset-5 col-md-4 col-md-offset-5" href="{{ url('/logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Cerrar Sesión</a>
 					<form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
