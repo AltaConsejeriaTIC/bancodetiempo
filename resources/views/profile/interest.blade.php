aa7343b9d8488be51cc2c86662d1d3d482940962
Cristian Daza <ccdaza2@gmail.com>
2017-01-13 14:34:52 -0500

    styles layout interest 100%

--- resources/views/profile/interest.blade.php
+++ resources/views/profile/interest.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <nav class='navbar-default navbar-static-top nav1 bannerInterest'>
-
+	<div class="cap"></div>
 	<div class="container">
 		<div class='row'>
 
@@ -10,6 +10,18 @@
 				<a href="/"> <img src="{{ asset('images/logo.png') }}" alt="Logo" />
 				</a>
 			</div>
+		@if((Auth::guest()))
+			<div class="hidden-xs col-sm-6 col-sm-offset-2 col-md-4 col-md-offset-5" id="container-nav-buttons">	    
+			     <button id="show-modal" @click="showModal = true" class="button5">{{ trans('dictionary.login') }}</button>          	
+			     <button id="show-modal" @click="showModal = true" class="button4 hidden-xs">Registrarse</button>          		          
+			</div>
+
+		@elseif((!Auth::guest()))
+			<a class="hidden-xs col-sm-4 col-sm-offset-5 col-md-4 col-md-offset-5" href="{{ url('/logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Cerrar SesiÃ³n</a>
+			<form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+				{{ csrf_field() }}
+			</form>
+		@endif
 
 		</div>
 	</div>
@@ -19,53 +31,42 @@
 	
 	<div class="container">
 		
-		<article class="col-xs-12">
+		<article class="col-xs-12" id='interestForm'>
 		
 			<div class="row">
 		
-				<h2 class='title1 col-xs-12'>¿Cuáles son tus intereses?</h2>
+				<h2 class='title1 col-xs-12'>¿Cuáles son tus intereses? <i class="fa fa-check-circle done" v-if='this.totalInterest >= 3'></i></h2>
 			
 			</div>
 			<div class="row">
 				<p class="paragraph1 col-xs-12">Elige <strong>tres</strong> categorías de interés para sugerirte ofertas.</p>
 			</div>
-			<div class="row"></div>
+			
+			{!! Form::open(['url' => '/profile/interest', 'method' => 'post', 'enctype' => 'multipart/form-data', 'class' => 'form-custom row', 'role' => 'form']) !!}
+											
+					@foreach($categories as $i => $category)
+						<div class='col-xs-12 col-md-4'>
+							{{ Form::checkbox('interets[]', $category->id, false, ['id' => str_replace(" ", "", $category->category), 'class' => 'boxCheck', '@click' => 'addInterest('.$i.')']) }}								
+							<label for="{{ str_replace(' ', '', $category->category) }}"><i class='fa fa-circle'></i>{{ $category->category }}</label>					
+						</div>	
+					@endforeach
+				
+				<div class='crearfix'></div>
+		
+				<div class="space40 col-xs-12 col-md-6 col-md-offset-3">
+					{{ Form::submit('Enviar', ['class' => 'button1 background-active-color col-xs-12', ':class' => '{inactive: this.totalInterest < 3}']) }}
+				</div>			
+		
+			{!! Form::close() !!}
 		
 		</article>
 		
-		
-	
-	
-		{!! Form::open(['url' => '/profile/interest', 'method' => 'post', 'enctype' => 'multipart/form-data', 'class' => 'form-custom col-xs-12', 'role' => 'form']) !!}
-				
-		<div id="temasInteres" class='row'>
-				
-				@foreach($categories as $i => $category)
-					
-					<div class=''>
-						
-						<div>
-						
-							{{ Form::checkbox('interets[]', $category->id, false, ['id' => str_replace(" ", "", $category->category)]) }}
-							
-							{{ Form::label(str_replace(" ", "", $category->category), $category->category) }}
-						
-						</div>
-						
-					</div>
-				
-				@endforeach
-							
-
-		</div>
-		
-		{{ Form::submit('Enviar', ['class' => '']) }}
-		
-		{!! Form::close() !!}
 
 	
 	</div>
 
 </section>
+
+ <script src="{{ url('js/interest.js') }}"></script> 
 
 @endsection
