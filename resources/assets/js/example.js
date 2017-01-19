fa6d0077e7344e8f90ddda543a9a47e7553638ac
alejandromunevar <alejandro.munevar@gmail.com>
2017-01-19 18:21:52 -0500

    Integration View Register Components Vue.js

--- resources/assets/js/example.js
+++ resources/assets/js/example.js
@@ -2,18 +2,18 @@ module.exports = {
     ProfileUser: function () {
         var profile = {
             data:{
-                firstName : '',
-                lastName : '',
-                gender: '',
-                day: '',
-                mounth: '',
-                year: '',
-                aboutMe: '',
+                firstName : windowvar.userJs["first_name"],
+                lastName : windowvar.userJs["last_name"],
+                gender: windowvar.userJs["gender"],
+                day: parseInt(windowvar.dayJs),
+                mounth: parseInt(windowvar.mounthJs),
+                year: parseInt(windowvar.yearJs),
+                aboutMe: windowvar.userJs["aboutMe"],
                 terms: false,
                 expr: new RegExp('^[^ ][a-zA-ZñÑáéíóúÁÉÍÓÚ ]*$'),
+                exprNum: new RegExp('^[^ ][a-zA-ZñÑáéíóúÁÉÍÓÚ0-9 ]*$'),
                 maxChar : 250,
-                totalChar: 250,
-                image:''
+                totalChar: 250
             }
         }
         return profile;
