74cc8be5f7bf2c59588f81180fbc792fc61b660a
alejandromunevar <alejandro.munevar@gmail.com>
2017-01-24 14:11:00 -0500

    changes in view profile

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -50,7 +50,7 @@
             <article class="col-md-8">
                 <div class="row">
                     @foreach($services as $key => $service)                  
-                      <div class='col-md-4 col-xs-12 col-sm-6'>                            
+                      <div class='col-md-6 col-xs-12 col-sm-6'>                            
                           @include('partial/serviceBox', array("service" => $service))                                            
                       </div>                      
                     @endforeach                    
