8dbd77b406b0564407e42b21383213714875582c
akod55 <akod55@gmail.com>
2017-01-26 16:32:31 -0500

    view welcome update like mockup

--- resources/assets/js/components/Modal.vue
+++ resources/assets/js/components/Modal.vue
@@ -3,16 +3,17 @@
         <div class="modal-mask" >
             <div class="modal-wrapper">
               <div class="modal-container">
-                <div class="close-modal">
+                <div class="close-modal ">
                   <button type="button" @click="$emit('close')" class="close circle-shape" data-dismiss="alert">&times;</button>
                 </div>
-                <div class="modal-header">
+                <div class="modal-header-welcome">
                     <h1 class="title1">Inicia sesión </h1>
                 </div>
-                <div class="modal-body">
+                <div class="modal-body-welcome">
                   <p class="paragraph2">
-                    Mensaje de Registro {{show}}
-                  </p>            
+                    Elige la opción que desees para ingresar.
+                  </p>
+                  <div class="modal-button-center">
                   <div class="row">
                     <div class="col-xs-12">
                       <a href="loginRedes/facebook" class="col-xs-12 col-md-12 button6 social-button facebook">Ingresa con Facebook <i class="fa fa-facebook"></i></a>
@@ -28,7 +29,8 @@
                     <div class="col-xs-12">
                       <a href="loginRedes/linkedin"  class="col-xs-12 col-md-12 button6 social-button linkedin">Ingresa con Linkedin <i class="fa fa-linkedin"></i></a>
                     </div>
-                  </div>          
+                  </div>
+                  </div>
                 </div>
                 <div class="modal-footer">        
                 </div>
--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -3,34 +3,9 @@
 	<div class="container">
 		
 		<div class="row">
-		
-			<div class='col-lg-2 col-md-2 col-sm-2 col-xs-6'>
-			
-				<ul>
-					<li><a href="">Home</a></li>
-					<li><a href="">Unete</a></li>
-					<li><a href="">Ingresa</a></li>
-				</ul>
-			
-			</div>
-			
-			<div class='col-lg-2 col-md-2 col-sm-3 col-xs-6'>
-			
-				<ul>
-					<li><a href="">Politicas de privacidad</a></li>
-					<li><a href="">Preguntas frecuentes</a></li>
-					<li><a href="">Contacto</a></li>
-				</ul>
-			
-			</div>
-			
-			
-			<div class='col-lg-4  col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-1 col-xs-12 col-xs-offset-0'>
-				Apoyan:<br>
-				<img src="" style="background: #333" alt="" />
-			</div>
-		
-		
+
+
+
 		</div>
 	
 	</div>
