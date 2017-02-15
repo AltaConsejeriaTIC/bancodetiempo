9b4898b2048bd2290c68208428395b2a4cc5ac02
Cristian Daza <ccdaza2@gmail.com>
2017-02-15 13:02:37 -0500

    conversation

--- resources/views/inbox.blade.php
+++ resources/views/inbox.blade.php
@@ -35,8 +35,40 @@
 						<div class='col-md-3'><h4></h4><p>{{str_limit($message->message, 30)}}</p></div>
 						<div class='col-md-2'><p>{{$message->created_at}}</p></div>
 						<div class='col-md-3'>
-							
+							<button type="button" class='button1 background-active-color' v-on:click='putMyData("message{{$message->id}}", true)'>Ver mensaje</button>							
 						</div>
+						<generalmodal name='message{{$message->id}}' :state='myData.message{{$message->id}}' state-init='false'>
+							<div slot="modal" class='box row'>
+								{!! Form::open(['url' => '/defaultsend/'.$message->conversation->service->id, 'method' => 'get', 'class' => 'form-custom col-md-10 col-md-offset-1']) !!} 
+								
+									<div class="row">
+										<div class="col-md-10 col-md-offset-1">
+											<p>{{$message->message}}</p>
+										</div>
+									</div>
+								
+									<div class="row">
+										<div class="col-md-10 col-md-offset-1">
+											<textarea name="content" class='validation ' id="content" rows="10" placeholder=''  data-validations='["required", "min:10", "max:250"]'></textarea>
+		      								<div class='clearfix'></div>
+										</div>
+									</div>
+								
+									<div class="row">
+					      				<div class="col-md-10 col-md-offset-1">
+					      					<input type='submit' value='Enviar' class='col-md-12 button1 background-active-color'>
+					      				</div>
+					      			</div>
+					      			<div class="space10"></div>
+					      			<div class="row">
+					      				<div class="col-md-10 col-md-offset-1">
+					      					<a class='button10 col-md-12 text-center'  v-on:click='putMyData("contactMail", false)'>Cancelar</a>
+					      				</div>
+					      			</div>
+								
+								{!! Form::close() !!}
+							</div>
+						</generalmodal>
 					</div>
 				@endforeach
 			
