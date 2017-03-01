a02db3a63d04d3abc6c6aeed34225673cff367d4
alejandromunevar <alejandro.munevar@gmail.com>
2017-03-01 11:01:36 -0500

    Fix conflict pull

--- app/Models/Deal.php
+++ app/Models/Deal.php
@@ -9,21 +9,20 @@ class Deal extends Model
 {
   protected  $fillable = ['user_id', 'service_id','date','time','location','value','description','state_id','observation'];
 
-<<<<<<< HEAD
+
   	public function conversation(){
-	
+
 		return $this->belongsTo(Conversations::class);
-	
+
 	}
 
 	public function observations(){
-	
+
 		return $this->hasMany(DealsObservations::class);
-	
-=======
+  }
+
   public function service()
-  {		
-		return $this->belongsTo(Service::class);		
->>>>>>> 56bd4e35c88410ece414484fe3e0dab03ed63e96
+  {
+		return $this->belongsTo(Service::class);
 	}
 }
