95955d6362c395f2ee2329d502fe6b5d20e799ad
Cristian Daza <ccdaza2@gmail.com>
2017-03-06 11:24:06 -0500

    validation deals exist

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -29,62 +29,63 @@
 @if($message->dealState != 8)
 	@include('deals/deal')			
 @endif
+@if(!is_null($conversation->deals->first()))
+    @if($conversation->applicant_id == Auth::User()->id)
 
-@if($conversation->applicant_id == Auth::User()->id)
-   @if(!is_null($conversation->deals->last()->user->user_score->where("user_evaluator_id", $conversation->service->user->id)->last()))
-        <div class="row">
-            <div class="col-md-12">
-                <h3 class="title2">{{$conversation->service->user->first_name}} te ha dejado el siguiente comentario:</h3>
+       @if(!is_null($conversation->deals->last()->user->user_score->where("user_evaluator_id", $conversation->service->user->id)->last()))
+            <div class="row">
+                <div class="col-md-12">
+                    <h3 class="title2">{{$conversation->service->user->first_name}} te ha dejado el siguiente comentario:</h3>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <p class="paragraph1">{{$conversation->deals->last()->user->user_score->where("user_evaluator_id", $conversation->service->user->id)->last()->observation}}</p>
+            <div class="row">
+                <div class="col-md-12">
+                    <p class="paragraph1">{{$conversation->deals->last()->user->user_score->where("user_evaluator_id", $conversation->service->user->id)->last()->observation}}</p>
+                </div>
             </div>
-        </div>
-    @endif
-    @if(!is_null($conversation->deals->last()->service->user->user_score->where("user_evaluator_id", $conversation->applicant->id)->last()))
-        <hr class="col-md-12">
-        <div class="row">
-            <div class="col-md-12">
-                <h3 class="title2">Escribiste el siguiente comentario para {{$conversation->service->user->first_name}}:</h3>
+        @endif
+        @if(!is_null($conversation->deals->last()->service->user->user_score->where("user_evaluator_id", $conversation->applicant->id)->last()))
+            <hr class="col-md-12">
+            <div class="row">
+                <div class="col-md-12">
+                    <h3 class="title2">Escribiste el siguiente comentario para {{$conversation->service->user->first_name}}:</h3>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <p class="paragraph1">{{$conversation->deals->last()->service->user->user_score->where("user_evaluator_id", $conversation->applicant->id)->last()->observation}}</p>
+            <div class="row">
+                <div class="col-md-12">
+                    <p class="paragraph1">{{$conversation->deals->last()->service->user->user_score->where("user_evaluator_id", $conversation->applicant->id)->last()->observation}}</p>
+                </div>
             </div>
-        </div>
-    @endif
+        @endif
 
-@else
-    @if(!is_null($conversation->deals->last()->service->user->user_score->where("user_evaluator_id", $conversation->applicant->id)->last()))
-        <div class="row">
-            <div class="col-md-12">
-                <h3 class="title2">{{$conversation->applicant->first_name}} te ha dejado el siguiente comentario:</h3>
+    @else
+        @if(!is_null($conversation->deals->last()->service->user->user_score->where("user_evaluator_id", $conversation->applicant->id)->last()))
+            <div class="row">
+                <div class="col-md-12">
+                    <h3 class="title2">{{$conversation->applicant->first_name}} te ha dejado el siguiente comentario:</h3>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <p class="paragraph1">{{$conversation->deals->last()->service->user->user_score->where("user_evaluator_id", $conversation->applicant->id)->last()->observation}}</p>
+            <div class="row">
+                <div class="col-md-12">
+                    <p class="paragraph1">{{$conversation->deals->last()->service->user->user_score->where("user_evaluator_id", $conversation->applicant->id)->last()->observation}}</p>
+                </div>
             </div>
-        </div>
-    @endif
-    @if(!is_null($conversation->deals->last()->user->user_score->where("user_evaluator_id", $conversation->service->user->id)->last()))
-        <hr class="col-md-12">
-        <div class="row">
-            <div class="col-md-12">
-                <h3 class="title2">Escribiste el siguiente comentario para {{$conversation->applicant->first_name}}:</h3>
+        @endif
+        @if(!is_null($conversation->deals->last()->user->user_score->where("user_evaluator_id", $conversation->service->user->id)->last()))
+            <hr class="col-md-12">
+            <div class="row">
+                <div class="col-md-12">
+                    <h3 class="title2">Escribiste el siguiente comentario para {{$conversation->applicant->first_name}}:</h3>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <p class="paragraph1">{{$conversation->deals->last()->user->user_score->where("user_evaluator_id", $conversation->service->user->id)->last()->observation}}</p>
+            <div class="row">
+                <div class="col-md-12">
+                    <p class="paragraph1">{{$conversation->deals->last()->user->user_score->where("user_evaluator_id", $conversation->service->user->id)->last()->observation}}</p>
+                </div>
             </div>
-        </div>
+        @endif
     @endif
 @endif
-
 
 
 <div class='row not-margin'>
