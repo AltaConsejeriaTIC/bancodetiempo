d85c2373b8c667ef396cf3cd59f83356d0cd74a9
Cristian Daza <ccdaza2@gmail.com>
2017-03-07 09:34:32 -0500

    bad observation 5%

--- resources/views/partial/observationForm.blade.php
+++ resources/views/partial/observationForm.blade.php
@@ -10,9 +10,6 @@
                     <p class="paragraph4 col-md-12 text-center">Oferta: {{$conversation->service->name}}</p>
                 </div>
                 <div class="row">
-                    <p class="paragraph4 text-center text-red col-md-12">Debes calificar este servicio para seguir ofertando y/o recibiendo servicios dentro de la plataforma.</p>
-                </div>
-                <div class="row">
                     @if($conversation->service->user->id == Auth::User()->id)
                         <h3 class="title2 col-md-12">Valora a {{$conversation->applicant->first_name}}</h3>
                     @elseif($conversation->applicant_id == Auth::User()->id)
@@ -63,6 +60,52 @@
                    <div class="col-md-12">
                        <textarea name="observation" class='validation' id="observation" cols="30" rows="10" placeholder="Ej. La calidad del servicio fue excelente, pasé un rato agradable y me divertí mucho aunque hubiera sido mejor comenzar con la actividad puntualmente." data-validations='["required", "min:10", "max:200"]'></textarea>
                    </div>
+                </div>
+                <div class="row">
+                   <div class="col-md-12">
+                     <input type="hidden" name='service_id' value='{{$conversation->service->id}}'>
+                      <input type="hidden" name='offerer_id' value='{{$conversation->service->user->id}}'>
+                      <input type="hidden" name='applicant_id' value='{{$conversation->applicant_id}}'>
+                      <input type="hidden" name='response' value='1'>
+                      <input type="hidden" name='deal_id' value=''>
+                       @if($conversation->service->user->id == Auth::User()->id)
+                          <input type="hidden" name="scoreFrom" value='offerer'>
+                        @elseif($conversation->applicant_id == Auth::User()->id)
+                          <input type="hidden" name="scoreFrom" value='applicant'>
+                        @endif
+                       <button type="submit" class='button1 background-active-color col-md-12'>Enviar</button>
+                   </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col-md-12">
+                       <button type="button" class='button10 background-white col-md-12 hiddenModal' modal='form-observation'>Cancelar</button>
+                   </div>
+                </div>
+                <br>
+
+            </div>
+        {!!Form::close()!!}
+    </div>
+</div>
+
+
+<div class='box-modal' id='form-bad-observation'>
+    <div class='shadow'></div>
+    <div class='panel box-fixed col-md-4 col-md-offset-4'>
+       {!! Form::open(['url' => '/addObservation', 'method' => 'post', 'class' => 'form-custom', 'v-validation:msg' => '']) !!}
+            <div class="content">
+                <div class="row">
+                    <h1 class="title1 col-md-10 col-md-offset-1">¿Qué salió mal?</h1>
+                </div>
+                <div class="row">
+                    <p class="paragraph4 col-md-12 text-center">Oferta: {{$conversation->service->name}}</p>
+                </div>
+                <div class="row">
+                    <p class="paragraph4 text-center text-red col-md-12">Elije el motivo por el cuál no se realizó el acuerdo.</p>
+                </div>
+                <div class="row">
+                    <p class="paragraph4 col-md-12">Amabilidad, respeto y confianza</p>
                 </div>
                 <div class="row">
                    <div class="col-md-12">
