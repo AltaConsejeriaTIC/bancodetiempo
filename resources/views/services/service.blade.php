ae988ea2c06ed2a89dbab204766739989b2be20d
Cristian Daza <ccdaza2@gmail.com>
2017-03-21 15:50:58 -0500

    fix tags detail services

--- resources/views/services/service.blade.php
+++ resources/views/services/service.blade.php
@@ -60,7 +60,7 @@
 								@if($service->virtually !== 0) Virtual @endif</p>
 							<p class="space15"></p>
 							<p class="paragraph4 text-bold"> Adquiere esta oferta por: </p>
-							<div class="row space10">
+							<div class="row">
 								<div class="col-md-2">
 									<img src="{{ asset('images/moneda.png') }}" class=" moneda icon-nav text-center"></image>
 								</div>
@@ -72,17 +72,20 @@
 						</div>
 
 					</div>
-					<div class='col-xs-12  col-sm-12'>
+					<div class="row">
+					    <div class='col-xs-12  col-sm-12'>
 
-						@foreach($service->tags as $tag)
-							@if($tag->service_id == $service->id)
-								<a class="col-xs-6 input-tag button7 tag-margin" tag='{{ $tag->tag->id }}' href='/filter?filter={{ $tag->tag->tag }}'>
-									<span>{{ $tag->tag->tag }}</span>
-								</a>
-							@endif
-						@endforeach
+                            @foreach($service->tags as $tag)
+                                @if($tag->service_id == $service->id)
+                                    <a class="col-xs-6 input-tag button7 tag-margin" tag='{{ $tag->tag->id }}' href='/filter?filter={{ $tag->tag->tag }}'>
+                                        <span>{{ $tag->tag->tag }}</span>
+                                    </a>
+                                @endif
+                            @endforeach
+                        </div>
 					</div>
-					<div>
+
+					<div class="row">
 						<hr class="  col-md-12 report_line hidden-xs">
 						@if(!is_null(Auth::User()))
 							<a class="report" href="" title="Reportar Contenido" data-toggle="modal" data-target="#update-dialog{{$service->id}}"><i class=" material-icons " aria-hidden="true">report</i>Reportar contenido</a>
