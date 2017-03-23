f78a0539ca202e7162fc75dc3a14854850930beb
akosoriod <akosoriod@unal.edu.co>
2017-03-23 14:59:57 -0500

    sedeers tags drop

--- database/seeds/TagsTableSeeder.php
+++ database/seeds/TagsTableSeeder.php
@@ -21,12 +21,6 @@ class TagsTableSeeder extends Seeder
     public function run()
     {
     	
-    	$faker = Faker\Factory::create('es_ES');
-    	for ($i=0; $i < 15; $i++)
-    	{
-    		$this->create([
-    				'tag' => $faker->word
-    		]);
-    	}
+
     }
 }
