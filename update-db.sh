d0d35aae12d8f16664020c33825ea12b1625ca59
Víctor Rodriguez <vrodmz@gmail.com>
2017-04-25 10:18:42 -0500

    Field 'confirmed' on campaign participants changed to boolean

--- update-db.sh
+++ update-db.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-php artisan migrate:rollback
+#php artisan migrate:rollback
+mysql -u root -ptoor -e "drop database bancodetiempo; create database bancodetiempo;"
 php artisan migrate
 mysql -u root -ptoor -f bancodetiempo < dummy-data.sql
