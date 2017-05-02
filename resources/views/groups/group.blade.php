7b45d4532c2fea39c527e8dd7db38eddb4404c86
Cristian Daza <ccdaza2@gmail.com>
2017-05-02 16:26:09 -0500

    style profile group

--- resources/views/groups/group.blade.php
+++ resources/views/groups/group.blade.php
@@ -10,26 +10,51 @@
 
     <div class="container">
 
-        <article class="col-md-8">
+        <article class="col-md-4 col-xs-12">
 
             <div class="row">
-                <div class="col-md-12 groupCover">
-                    <img src="/{{$group->image}}" class='groupCover' alt="{{$group->name}}">
+                <div class="col-xs-6 col-xs-offset-3 col-md-6 col-md-offset-3 groupCover">
+                   @include('partial/imageProfile', array('cover' => $group->image, 'id' =>$group->id, 'border' => '#0f6784', 'borderSize' => '3px'))
                 </div>
             </div>
             <div class="row">
-                <div class="col-md 12">
-                    <h2 class="title title2-service">{{$group->name}}</h2>
+                <div class="col-xs-12">
+                    <h2 class="title text-center">{{$group->name}}</h2>
                 </div>
             </div>
-            <hr>
             <div class="row">
-                <div class="col-md 12">
+                <div class="col-xs-12">
                     <p class="paragraph4">{{$group->description}}</p>
                 </div>
             </div>
             <br>
             <div class="row">
+                <div class="col-md-12">
+                    <h2 class="title1">{{ trans("groups.collaborators") }}</h2>
+                </div>
+            </div>
+            <div class="row">
+                @foreach($group->collaborators as $k => $collaborator)
+
+                    <div class="col-md-6">
+                        <div class="col-md-8 col-md-offset-2">
+                            @include('partial/imageProfile', array('cover' => $collaborator->user->avatar, 'id' =>$collaborator->user->id, 'border' => '#0f6784', 'borderSize' => '3px'))
+                        </div>
+                        <div class="clearfix"></div>
+                        <p class="paragraph2 text-center">{{ $collaborator->user->first_name ." ". $collaborator->user->last_name }}</p>
+                    </div>
+                    @if($k%2 != 0)
+                        <div class="clearfix"></div>
+                    @endif
+
+                @endforeach
+            </div>
+
+        </article>
+
+        <article class="col-md-8">
+
+           <div class="row">
                 <div class="col-md 12">
                     <h2 class="title title2-service">{{ trans("groups.campaign") }}</h2>
                 </div>
@@ -57,31 +82,6 @@
                 @endforeach
             </div>
 
-        </article>
-
-        <article class="col-md-4">
-
-            <div class="row">
-                <div class="col-md-12">
-                    <h2 class="title1">{{ trans("groups.collaborators") }}</h2>
-                </div>
-            </div>
-            <div class="row">
-                @foreach($group->collaborators as $k => $collaborator)
-
-                    <div class="col-md-6">
-                        <div class="col-md-8 col-md-offset-2">
-                            @include('partial/imageProfile', array('cover' => $collaborator->user->avatar, 'id' =>$collaborator->user->id, 'border' => '#0f6784', 'borderSize' => '3px'))
-                        </div>
-                        <div class="clearfix"></div>
-                        <p class="paragraph2 text-center">{{ $collaborator->user->first_name ." ". $collaborator->user->last_name }}</p>
-                    </div>
-                    @if($k%2 != 0)
-                        <div class="clearfix"></div>
-                    @endif
-
-                @endforeach
-            </div>
 
         </article>
 
