a56e291d2f07e13c320c208aca4d4dbf604003cb
Cristian Daza <ccdaza2@gmail.com>
2017-05-12 11:58:36 -0500

    change send mail

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -49,7 +49,7 @@ class EmailController extends Controller
     	$content = $request->input('content');
 
     	Mail::send('mailContact',["service" => $service, "userauth" => $userauth, "myImageProfile" => $myImageProfile, "imageProfile" => $imageProfile, "imageService" => $imageService,'content' => $content], function ($message) use ($mail){
-    		$message->from('evenvivelab_bog@unal.edu.co','Cambalachea!');
+    		$message->from('bancodetiempo@cambalachea.co','Cambalachea!');
     		$message->subject('Notificación');
     		$message->to($mail);
     	});
