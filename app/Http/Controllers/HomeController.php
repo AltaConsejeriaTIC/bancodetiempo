c681f280eef6a8ad1fd27247d794fce7275314c6
Víctor Rodriguez <vrodmz@gmail.com>
2017-05-18 16:08:15 -0500

    Showing services as a list

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Groups;
 use Illuminate\Http\Request;
 use App\Models\Service;
 use App\Models\InterestUser;
@@ -16,81 +17,90 @@ use App\Models\subscribers;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Session;
 
-class HomeController extends Controller{
+class HomeController extends Controller
+{
 
-    public function indexNotRegister(){
+    public function indexNotRegister()
+    {
 
-    	if(Auth::check()){
-    		return redirect('/home');
-    	}else{
-    		$lastServices = Service::getServicesActive()->get()->take(6);
-    		return view('welcome', compact('lastServices'));
-    	}
+        if (Auth::check()) {
+            return redirect('/home');
+        } else {
+            $lastServices = Service::getServicesActive()->get()->take(6);
+            return view('welcome', compact('lastServices'));
+        }
 
     }
 
-    public function index(Request $request){
+    public function index(Request $request)
+    {
 
         $categories = Category::getCategoriesInUse();
         $allServices = Service::getServicesActive()->paginate(12);
         $serviceAdmin = ServiceAdmin::getServicesActive()->paginate(12);
         $recommendedServices = User::recommendedServices();
+        $groups = Groups::getActiveGroups()->paginate(12);
         JavaScript::put([
             'categoriesJs' => $categories,
         ]);
-        $campaigns = Campaigns::where('state_id', 1)->get()->where("groups.state_id", 1);
+        $campaigns = Campaigns::select("campaigns.*")->where('campaigns.state_id', 1)->join("groups","groups.id", "campaigns.groups_id")->where("groups.state_id", 1)->paginate(12);
 
-        return view('home', compact('allServices', 'recommendedServices', 'categories', 'campaigns', 'serviceAdmin'));
+        return view('home', compact('allServices', 'recommendedServices', 'categories', 'campaigns', 'serviceAdmin', 'groups'));
     }
 
-    
-    public function filter(Request $request){
+
+    public function filter(Request $request)
+    {
 
         $categories = Category::getCategoriesInUse();
-    	$allServices = ServiceController::getServiceActive();
+        $allServices = ServiceController::getServiceActive();
         $this->filterTags($allServices, $request->input('filter'));
-    	$allServices = $allServices->paginate(12);
-    	$recommendedServices = User::recommendedServices();
+        $allServices = $allServices->paginate(12);
+        $recommendedServices = User::recommendedServices();
         $serviceAdmin = ServiceAdmin::getServicesActive()->paginate(12);
-    	JavaScript::put([
-    			'categoriesJs' => $categories
-    	]);
-        $campaigns = Campaigns::where('state_id', 1)->get()->where("groups.state_id", 1);
-    	return view('home', compact('allServices', 'recommendedServices', 'categories', 'campaigns', 'serviceAdmin'));
+        $groups = Groups::getActiveGroups()->paginate(12);
+        JavaScript::put([
+            'categoriesJs' => $categories
+        ]);
+        $campaigns = Campaigns::select("campaigns.*")->where('campaigns.state_id', 1)->join("groups","groups.id", "campaigns.groups_id")->where("groups.state_id", 1)->paginate(12);
+        return view('home', compact('allServices', 'recommendedServices', 'categories', 'campaigns', 'serviceAdmin', 'groups'));
     }
 
-    public function filterTags($allServices, $filter){
+    public function filterTags($allServices, $filter)
+    {
 
-        if($filter != ""){
-			$filters = explode(" ", $filter);
+        if ($filter != "") {
+            $filters = explode(" ", $filter);
             $idTags = Tag::select("id")->whereIn('tag', $filters)->get();
             $allServices->join('tags_services', 'services.id', 'tags_services.service_id');
             Session::flash('filters.tags', $idTags);
-    		return $allServices->WhereIn('tags_services.tag_id', $idTags);
-		}else{
-			Session::pull('filters.tags');
+            return $allServices->WhereIn('tags_services.tag_id', $idTags);
+        } else {
+            Session::pull('filters.tags');
             return $allServices;
-		}
+        }
 
     }
 
-    public function filterText($allServices, $filter){
+    public function filterText($allServices, $filter)
+    {
 
-        if($filter != ""){
-    		return $allServices->where('services.name', 'LIKE', "%$filter%");
+        if ($filter != "") {
+            return $allServices->where('services.name', 'LIKE', "%$filter%");
 
-		}else{
+        } else {
             return $allServices;
-		}
+        }
 
     }
 
-    public function subscribe(Request $request){
+    public function subscribe(Request $request)
+    {
 
         $this->validate($request, [
             'email' => 'required|email',
         ]);
-        if(is_null(subscribers::where("email", $request->input('email'))->get()->last())){
+        if (is_null(subscribers::where("email", $request->input('email'))->get()->last())) {
             subscribers::create([
                 "email" => $request->input('email')
             ]);
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,122 +1,92 @@
 @extends('layouts.app')
 @section('content')
 
-	@if(!is_null(Auth::User()))
-		@include('nav',array('type' => 2))
-	@else
-		@include('nav',array('type' => 3))
-	@endif
+    @if(!is_null(Auth::User()))
+        @include('nav',array('type' => 2))
+    @else
+        @include('nav',array('type' => 3))
+    @endif
 
-	<div class="container">
-		<div class="row">
-		    <ul  class="nav nav-pills col-md-8 col-xs-12">
-				<li class="active">
-		        	<a  href="#filterAll" data-toggle="tab" class='parrafo3' v-on:click='filterCategory(0)'>{{ trans('home.all') }}</a>
-				</li>
-				<li class="hidden">
-		        	<a  href="#filterRecommended" data-toggle="tab" class='parrafo3'>{{ trans('home.Recommended') }}</a>
-				</li>
-				<li>
-				    <a  href="#filterCampaign" data-toggle="tab" class='parrafo3'>{{ trans('home.campaigns') }}</a>
-				</li>
-				<li>
-				    <a  href="#filterAdmin" data-toggle="tab" class='parrafo3'>{{ trans('home.serviceAdmin') }}</a>
-				</li>
-			</ul>
-			<div class='col-md-1 col-xs-5 not-padding'>
-				<p class="paragraph10">{{ trans('home.filter') }}</p>
-			</div>
-			<div class="col-md-3 col-xs-7">
-				<filters-categories categories='{{ $categories }}'></filters-categories>
-			</div>
-		</div>
-		<br>
+    <div class="container">
+        <div class="row">
+            <ul class="nav nav-pills col-md-8 col-xs-12">
+                <!--li class="active">
+                    <a href="#filterAll" data-toggle="tab" class='parrafo3'
+                       v-on:click='filterCategory(0)'>{{ trans('home.all') }}</a>
+                </li>
+                <li class="hidden">
+                    <a href="#filterRecommended" data-toggle="tab" class='parrafo3'>{{ trans('home.Recommended') }}</a>
+                </li>
+                <li>
+                    <a href="#filterCampaign" data-toggle="tab" class='parrafo3'>{{ trans('home.campaigns') }}</a>
+                </li>
+                <li>
+                    <a href="#filterAdmin" data-toggle="tab" class='parrafo3'>{{ trans('home.serviceAdmin') }}</a>
+                </li-->
+            </ul>
+            <div class='col-md-1 col-xs-5 not-padding'>
+                <p class="paragraph10">{{ trans('home.filter') }}</p>
+            </div>
+            <div class="col-md-3 col-xs-7">
+                <filters-categories categories='{{ $categories }}'></filters-categories>
+            </div>
+        </div>
+        <br>
 
-		@if(count(Illuminate\Support\Facades\Session::get("filters.tags", [])) > 0)
+        @if(count(Illuminate\Support\Facades\Session::get("filters.tags", [])) > 0)
             <div class="row">
                 <div class="col-xs-12">
                     <strong class="paragraph1">Filtros: </strong>
                     @foreach(Illuminate\Support\Facades\Session::get("filters.tags", []) as $tag)
-                        <a tag="{{$tag->id}}" class="input-tag button7 tag-margin"><span>{{ App\Models\Tag::find($tag->id)->tag }}</span></a>
+                        <a tag="{{$tag->id}}"
+                           class="input-tag button7 tag-margin"><span>{{ App\Models\Tag::find($tag->id)->tag }}</span></a>
                     @endforeach
                     <a href='/filter'>Borrar filtros</a>
                 </div>
 
             </div>
         @endif
-		<div class='row'>
+        <div class='row'>
+            <div class='row'>
+                <h1 class="parrafo3">{{ trans('home.services') }}</h1>
+                @foreach($allServices as $key => $service)
 
-            <div class="tab-content clearfix col-xs-12">
-                <div class="tab-pane active" id="filterAll">
-                  <div class='row'>
-                    @foreach($allServices as $key => $service)
-
-                      <div class='col-md-4 col-xs-12 col-sm-6'>
-                          @include('partial/serviceBox', array("service" => $service))
-                      </div>
-
-                    @endforeach
+                    <div class='col-md-4 col-xs-12 col-sm-6'>
+                        @include('partial/serviceBox', array("service" => $service))
                     </div>
-                    <div class="text-center">
-                        {!! $allServices->links('vendor.pagination.bootstrap-4') !!}
-                    </div>
+
+                @endforeach
+                <div class="text-center">
+                    {!! $allServices->links('vendor.pagination.bootstrap-4') !!}
                 </div>
+            </div>
+            <div class='row'>
+                <h1 class="parrafo3">{{ trans('home.groups') }}</h1>
+                @foreach($groups as $key => $group)
 
-
-                <div class="tab-pane hidden" id="filterRecommended">
-                    <div class='row'>
-                        @if($recommendedServices != '')
-                            @foreach($recommendedServices as $key => $service)
-
-                              <div class='col-md-4 col-xs-12 col-sm-6'>
-
-                                  @include('partial/serviceBox', array("service" => $service))
-
-                              </div>
-
-                            @endforeach
-                         @endif
+                    <div class='col-md-4 col-xs-12 col-sm-6'>
+                        @include('partial/groupBox')
                     </div>
+
+                @endforeach
+                <div class="text-center">
+                    {!! $groups->links('vendor.pagination.bootstrap-4') !!}
                 </div>
+            </div>
+            <div class='row'>
+                <h1 class="parrafo3">{{ trans('home.campaigns') }}</h1>
+                @foreach($campaigns as $key => $campaign)
 
-                <div class="tab-pane" id="filterCampaign">
-                    <div class='row'>
-                        @if($campaigns != '')
-                            @foreach($campaigns as $key => $campaign)
-
-                              <div class='col-md-4 col-xs-12 col-sm-6'>
-
-                                  @include('partial/campaignBox')
-
-                              </div>
-
-                            @endforeach
-                         @endif
+                    <div class='col-md-4 col-xs-12 col-sm-6'>
+                        @include('partial/campaignBox', array("service" => $campaign))
                     </div>
+
+                @endforeach
+                <div class="text-center">
+                    {!! $campaigns->links('vendor.pagination.bootstrap-4') !!}
                 </div>
-
-                <div class="tab-pane" id="filterAdmin">
-                    <div class='row'>
-
-                            @foreach($serviceAdmin as $key => $service)
-
-                              <div class='col-md-4 col-xs-12 col-sm-6'>
-
-                                  @include('partial/serviceBox', array("service" => $service))
-
-                              </div>
-
-                            @endforeach
-
-                    </div>
-                    <div class="text-center">
-                        {!! $serviceAdmin->links('vendor.pagination.bootstrap-4') !!}
-                    </div>
-                </div>
-
-	       </div>
-	    </div>
-
+            </div>
+        </div>
     </div>
 
 
