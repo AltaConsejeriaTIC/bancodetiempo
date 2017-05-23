a2b3c4f2964b4046669d342274121bd363e275da
Víctor Rodriguez <vrodmz@gmail.com>
2017-05-23 15:40:14 -0500

    Fixed css bug with people list

--- resources/views/persons/persons.blade.php
+++ resources/views/persons/persons.blade.php
@@ -14,42 +14,33 @@
                     <h1 class="panel-title title1">{{ trans('home.persons') }}</h1>
                 </div>
                 <div class="panel-body">
-                    @php($i = 0)
-                    @php($k = 0)
+                    <div class="row">
+                        @php($i = 0)
 
-                    @while($i < 4 && $k < sizeof($persons) )
+                        @while($i < sizeof($persons) )
+                            @php($person = $persons[$i])
 
-                        <div class="row">
-                            @php($j = 0)
-
-                            @while($j < 3 && $k < sizeof($persons))
-                                @php($person = $persons[$k])
-
-                                <a href="/user/{{$person->id}}">
-                                    <div class='col-md-4 col-xs-12 col-sm-6'>
-                                        <div style="width: 80px">
-                                            @include('partial/imageProfile', array('cover' => $person->avatar, 'id' =>$person->id, 'border' => '#0f6784', 'borderSize' => '3px'))
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-xs-12">
-                                                <h2 class="title2">{{$person->first_name." ".$person->last_name}}</h2>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-xs-12">
-                                                <p class='paragraph4'>{{$person->aboutMe}}</p>
-                                            </div>
+                            <a href="/user/{{$person->id}}">
+                                <div class='col-md-4 col-xs-12 col-sm-6' style="min-height: 200px">
+                                    <div style="width: 80px">
+                                        @include('partial/imageProfile', array('cover' => $person->avatar, 'id' =>$person->id, 'border' => '#0f6784', 'borderSize' => '3px'))
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-xs-12">
+                                            <h2 class="title2">{{$person->first_name." ".$person->last_name}}</h2>
                                         </div>
                                     </div>
-                                </a>
-
-                                @php($k++)
-                                @php($j++)
-                            @endwhile
+                                    <div class="row">
+                                        <div class="col-xs-12">
+                                            <p class='paragraph4'>{{$person->aboutMe}}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
 
                             @php($i++)
-                        </div>
-                    @endwhile
+                        @endwhile
+                    </div>
 
                     <div class="row">
                         <div class="col-xs-12">
