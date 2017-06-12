27f06355baa197dd7c7b575bd07ac3b2a9e8ee71
Víctor Rodriguez <vrodmz@gmail.com>
2017-06-12 15:19:26 -0500

    Google analytics implemented

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,6 +39,16 @@
                 'csrfToken' => csrf_token(),
             ]); ?>
         </script>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-100857967-1', 'auto');
+            ga('send', 'pageview');
+
+        </script>
 
     </head>
 
@@ -51,11 +61,11 @@
         </div>
     </body>
 
-    <!--  Scripts-->    
+    <!--  Scripts-->
 
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('js/materialize.js') }}"></script>
-    <script src="{{ asset('js/app.js') }}"></script>    
+    <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/jquery-ui.js') }}"></script>
     <script>
         var date = new Date();
