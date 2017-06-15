e611a0a8a60fa84b97e264a9e08ea2c7c51ea4d9
Víctor Rodriguez <vrodmz@gmail.com>
2017-06-15 12:52:51 -0500

    Analitics for login and signup

--- app/Http/Controllers/NetworkAccountsController.php
+++ app/Http/Controllers/NetworkAccountsController.php
@@ -18,217 +18,229 @@ use Illuminate\Support\Facades\Session;
 
 class NetworkAccountsController extends Controller
 {
-	public function login($provider = ''){
+    public function login($provider = '')
+    {
 
-		if(!$provider || $provider == ''){
-			return Redirect::route("login");
-		}
-		Session::put('last_url', str_replace("http://".$_SERVER['HTTP_HOST'], "", URL::previous()));
-		$function = "redirect".ucwords($provider);
-		return $this->$function();
+        if (!$provider || $provider == '') {
+            return Redirect::route("login");
+        }
+        Session::put('last_url', str_replace("http://" . $_SERVER['HTTP_HOST'], "", URL::previous()));
+        $function = "redirect" . ucwords($provider);
+        return $this->$function();
 
-	}
-
-	public function validateLogout(){
-		if(auth()->user()){
-			auth()->logout();
-			return redirect('/');
-		}else{
-			return redirect('/');
-		}
     }
 
-	public function showFrom(){
+    public function validateLogout()
+    {
+        if (auth()->user()) {
+            auth()->logout();
+            return redirect('/');
+        } else {
+            return redirect('/');
+        }
+    }
 
-		$user = User::findOrFail(Auth::user()->id);
+    public function showFrom()
+    {
 
-		JavaScript::put([
-			'userJs'=> $user,			
-			'dayJs' => is_null($user->birthDate) ? "0" :  date("d",strtotime($user->birthDate)),
-			'mounthJs' => is_null($user->birthDate) ? "0" :  date("m",strtotime($user->birthDate)),
-			'yearJs' => is_null($user->birthDate) ? "0" :  date("Y",strtotime($user->birthDate)),
+        $user = User::findOrFail(Auth::user()->id);
 
-		]);
+        JavaScript::put([
+            'userJs' => $user,
+            'dayJs' => is_null($user->birthDate) ? "0" : date("d", strtotime($user->birthDate)),
+            'mounthJs' => is_null($user->birthDate) ? "0" : date("m", strtotime($user->birthDate)),
+            'yearJs' => is_null($user->birthDate) ? "0" : date("Y", strtotime($user->birthDate)),
 
-		return view('auth/register', compact('user'));
-	}
+        ]);
 
-	public function createUser($providerData){
+        return view('auth/register', compact('user'));
+    }
 
-		$account = new NetworkAccounts([
-				'provider_id' => $providerData['id'],
-				'provider' => $providerData['provider']
-		]);
+    public function createUser($providerData)
+    {
 
-        $email = is_null($providerData['email']) ? $providerData['id']."@cambalachea.co" : $providerData['email'];
+        $account = new NetworkAccounts([
+            'provider_id' => $providerData['id'],
+            'provider' => $providerData['provider']
+        ]);
 
-		$user = User::whereEmail($email)->first();
+        $email = is_null($providerData['email']) ? $providerData['id'] . "@cambalachea.co" : $providerData['email'];
 
-		if (!$user) {
+        $user = User::whereEmail($email)->first();
 
-			$user = User::create([
-					'email' => $email,
-					'email2' => $providerData['email'],
-					'first_name' => $providerData['first_name'],
-					'last_name' => $providerData['last_name'],
-					'avatar' => '',
-					'state_id' => 4,
-					'gender' => $providerData['gender'],
-					'birthDate' => $providerData['birthdate'] == '' ? NULL : date("Y-m-d", strtotime($providerData['birthdate'])),
-					'aboutMe' => '',
-					'role_id' => 2
-			]);
-		}
+        if (!$user) {
 
-		$account->user()->associate($user);
-		$account->save();
+            $user = User::create([
+                'email' => $email,
+                'email2' => $providerData['email'],
+                'first_name' => $providerData['first_name'],
+                'last_name' => $providerData['last_name'],
+                'avatar' => '',
+                'state_id' => 4,
+                'gender' => $providerData['gender'],
+                'birthDate' => $providerData['birthdate'] == '' ? NULL : date("Y-m-d", strtotime($providerData['birthdate'])),
+                'aboutMe' => '',
+                'role_id' => 2
+            ]);
+        }
 
-		auth()->login($user);	
+        $account->user()->associate($user);
+        $account->save();
 
-		$avatar = $this->getAvatar($providerData['avatar']);
-        if($avatar){
+        auth()->login($user);
+
+        $avatar = $this->getAvatar($providerData['avatar']);
+        if ($avatar) {
             $user->update([
                 'avatar' => $avatar
             ]);
         }
 
 
-		return true;
+        return true;
 
-	}
+    }
 
-    public function getAvatar($url){
-        if(!is_dir('resources/user/user_'.Auth::id())){
-            mkdir('resources/user/user_'.Auth::id(), 0777, true);
+    public function getAvatar($url)
+    {
+        if (!is_dir('resources/user/user_' . Auth::id())) {
+            mkdir('resources/user/user_' . Auth::id(), 0777, true);
         }
-        $img = 'resources/user/user_'.Auth::id().'/img'.Auth::id().'.jpg';
-        $file = fopen($img,"w+");
-        if($file != false){
-          file_put_contents($img, file_get_contents($url));
-          return $img;
-        }else{
+        $img = 'resources/user/user_' . Auth::id() . '/img' . Auth::id() . '.jpg';
+        $file = fopen($img, "w+");
+        if ($file != false) {
+            file_put_contents($img, file_get_contents($url));
+            return $img;
+        } else {
             return false;
         }
 
     }
 
-	public function callback($provider){
+    public function callback($provider)
+    {
 
-		if(!$provider || $provider == '' || Input::get("error")){
-			return Redirect::route("login", array('message' => "Error en la autenticaci&ntilde;n"));
-		}
+        if (!$provider || $provider == '' || Input::get("error")) {
+            return Redirect::route("login", array('message' => "Error en la autenticaci&ntilde;n"));
+        }
 
-		$function = "getProviderData".ucwords($provider);
+        $function = "getProviderData" . ucwords($provider);
 
-		$providerData = $this->$function();
+        $providerData = $this->$function();
 
-		$networkAccounts = new NetworkAccounts();
+        $networkAccounts = new NetworkAccounts();
 
-		$networkAccounts->start($providerData);
+        $networkAccounts->start($providerData);
 
-		if($networkAccounts->existsUser())
-		{
+        if ($networkAccounts->existsUser()) {
 
-			auth()->login($networkAccounts->getUser());
-			
-			if(auth()->user()->privacy_policy == 0){
-				return redirect('register');
-			}else{
-				
-				return Redirect::to(Session::get('last_url'));
-			}
+            Session()->put('GAEvent', ['event' => 'login', 'provider' => $provider]);
 
-		}else{
+            auth()->login($networkAccounts->getUser());
 
-			if($this->createUser($providerData)){
+            if (auth()->user()->privacy_policy == 0) {
+                return redirect('register');
+            } else {
+                return Redirect::to(Session::get('last_url'));
+            }
 
-				return redirect('register');
+        } else {
 
-			}
+            if ($this->createUser($providerData)) {
 
-		}
+                return redirect('register');
 
-	}
+            }
 
-	private function redirectFacebook(){
+        }
 
-		return Socialite::driver('facebook')->fields([
-				'first_name', 'last_name', 'email', 'gender', 'birthday'
-		])->scopes([
-				'email', 'user_birthday'
-		])->redirect();
+    }
 
-	}
+    private function redirectFacebook()
+    {
 
-	private function redirectGoogle(){
-		return Socialite::driver('google')->redirect();
-	}
+        return Socialite::driver('facebook')->fields([
+            'first_name', 'last_name', 'email', 'gender', 'birthday'
+        ])->scopes([
+            'email', 'user_birthday'
+        ])->redirect();
 
-	private function redirectLinkedin(){
-		return Socialite::driver('linkedin')->fields([
-				'first_name', 'last_name', 'email', 'gender', 'birthday'
-		])->redirect();
-	}
+    }
 
-	private function getProviderDataFacebook(){
+    private function redirectGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
 
-		$providerData = Socialite::driver("facebook")->fields([
-				'first_name', 'last_name', 'email', 'gender', 'birthday'
-		])->user();
+    private function redirectLinkedin()
+    {
+        return Socialite::driver('linkedin')->fields([
+            'first_name', 'last_name', 'email', 'gender', 'birthday'
+        ])->redirect();
+    }
 
-		$newProviderData = [
-				"first_name" => isset($providerData->getRaw()["first_name"]) ? $providerData->getRaw()["first_name"] : "",
-				"last_name" =>  isset($providerData->getRaw()["last_name"]) ? $providerData->getRaw()["last_name"] : "",
-				"email" =>  $providerData->getEmail(),
-				"birthdate" =>  isset($providerData->getRaw()["birthday"]) ? $providerData->getRaw()["birthday"] : "",
-				"gender" =>  isset($providerData->getRaw()["gender"]) ? $providerData->getRaw()["gender"] : "",
-				"id" =>  $providerData->getId(),
-				"provider" => "facebook",
-				"avatar" => $providerData->avatar_original
+    private function getProviderDataFacebook()
+    {
 
-		];
+        $providerData = Socialite::driver("facebook")->fields([
+            'first_name', 'last_name', 'email', 'gender', 'birthday'
+        ])->user();
 
-		return $newProviderData;
+        $newProviderData = [
+            "first_name" => isset($providerData->getRaw()["first_name"]) ? $providerData->getRaw()["first_name"] : "",
+            "last_name" => isset($providerData->getRaw()["last_name"]) ? $providerData->getRaw()["last_name"] : "",
+            "email" => $providerData->getEmail(),
+            "birthdate" => isset($providerData->getRaw()["birthday"]) ? $providerData->getRaw()["birthday"] : "",
+            "gender" => isset($providerData->getRaw()["gender"]) ? $providerData->getRaw()["gender"] : "",
+            "id" => $providerData->getId(),
+            "provider" => "facebook",
+            "avatar" => $providerData->avatar_original
 
-	}
+        ];
 
-	private function getProviderDataGoogle(){
+        return $newProviderData;
 
-		$providerData = Socialite::driver("google")->user();
+    }
 
-		$newProviderData = [
-				"first_name" => isset($providerData->getRaw()["name"]["givenName"]) ? $providerData->getRaw()["name"]["givenName"] : "",
-				"last_name" =>  isset($providerData->getRaw()["name"]["familyName"]) ? $providerData->getRaw()["name"]["familyName"] : "",
-				"email" =>  $providerData->getEmail(),
-				"birthdate" =>  "",
-				"gender" =>  isset($providerData->getRaw()["gender"]) ? $providerData->getRaw()["gender"] : "",
-				"id" =>  $providerData->getId(),
-				"provider" => "google",
-				"avatar" => $providerData->avatar_original
+    private function getProviderDataGoogle()
+    {
 
-		];
+        $providerData = Socialite::driver("google")->user();
 
-		return $newProviderData;
+        $newProviderData = [
+            "first_name" => isset($providerData->getRaw()["name"]["givenName"]) ? $providerData->getRaw()["name"]["givenName"] : "",
+            "last_name" => isset($providerData->getRaw()["name"]["familyName"]) ? $providerData->getRaw()["name"]["familyName"] : "",
+            "email" => $providerData->getEmail(),
+            "birthdate" => "",
+            "gender" => isset($providerData->getRaw()["gender"]) ? $providerData->getRaw()["gender"] : "",
+            "id" => $providerData->getId(),
+            "provider" => "google",
+            "avatar" => $providerData->avatar_original
 
-	}
+        ];
 
-	private function getProviderDataLinkedin(){
+        return $newProviderData;
 
-		$providerData = Socialite::driver("linkedin")->user();
+    }
 
-		$newProviderData = [
-				"first_name" => isset($providerData->getRaw()["firstName"]) ? $providerData->getRaw()["firstName"] : "",
-				"last_name" =>  isset($providerData->getRaw()["lastName"]) ? $providerData->getRaw()["lastName"] : "",
-				"email" =>  $providerData->getEmail(),
-				"birthdate" =>  isset($providerData->getRaw()["birthday"]) ? $providerData->getRaw()["birthday"] : "",
-				"gender" =>  isset($providerData->getRaw()["gender"]) ? $providerData->getRaw()["gender"] : "",
-				"id" =>  $providerData->getId(),
-				"provider" => "linkedin",
-				"avatar" => $providerData->avatar_original
+    private function getProviderDataLinkedin()
+    {
 
-		];
+        $providerData = Socialite::driver("linkedin")->user();
 
-		return $newProviderData;
+        $newProviderData = [
+            "first_name" => isset($providerData->getRaw()["firstName"]) ? $providerData->getRaw()["firstName"] : "",
+            "last_name" => isset($providerData->getRaw()["lastName"]) ? $providerData->getRaw()["lastName"] : "",
+            "email" => $providerData->getEmail(),
+            "birthdate" => isset($providerData->getRaw()["birthday"]) ? $providerData->getRaw()["birthday"] : "",
+            "gender" => isset($providerData->getRaw()["gender"]) ? $providerData->getRaw()["gender"] : "",
+            "id" => $providerData->getId(),
+            "provider" => "linkedin",
+            "avatar" => $providerData->avatar_original
 
-	}
+        ];
+
+        return $newProviderData;
+
+    }
 }
