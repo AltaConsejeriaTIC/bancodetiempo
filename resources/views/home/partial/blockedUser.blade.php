e3e5e9063b7746796ca251fa770a13bf406fdcb8
Víctor Rodriguez <vrodmz@gmail.com>
2017-07-17 11:54:20 -0500

    Showing a message to the blocked user

--- resources/views/home/partial/blockedUser.blade.php
+++ resources/views/home/partial/blockedUser.blade.php
@@ -7,12 +7,11 @@
             </div>
             <div class="text-center">
                 <h1 class="title1">Usuario Bloqueado</h1>
-                <p class="paragraph1" style="color: #001738;">
-                    Tu cuenta ha sido bloqueada. Por favor, comunícate con nosotros a <a
+                <p class="paragraph1 secondary-color">Tu cuenta ha sido bloqueada.<br/>Por favor, comunícate con nosotros a <a
                             href="mailto:info@cambalachea.co"
-                            style="color:#445870; text-decoration: underline !important;">info@cambalachea.co</a>
+                            class="link">info@cambalachea.co</a>
                 </p>
-                <div class="">
+                <div>
                     <i class="material-icons" style="font-size: 132px">block</i>
                 </div>
             </div>
