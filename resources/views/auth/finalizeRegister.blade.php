b21371a45ebbb518310752f120cac4ab4764e5ab
Cristian Daza <ccdaza2@gmail.com>
2017-08-01 11:57:55 -0500

    do not allow to create services to users with pending status

--- resources/views/auth/finalizeRegister.blade.php
+++ resources/views/auth/finalizeRegister.blade.php
@@ -2,66 +2,7 @@
 
 @section('content')
 
-<nav class='navbar navbar-default navbar-static-top nav2'>
-    <div class="container">
-        <div class='row flex-center'>
-
-            <div class="col-xs-7  col-sm-3  col-md-3">
-                <a href="/index">
-                    <img class="iconbar2" src="{{ asset('images/logo2.png') }}" alt="Logo"/>
-                </a>
-            </div>
-
-            <div class='hidden-xs col-xs-offset-6 col-xs-1 col-sm-1 text-right icon-profile padding-top'>
-                @include('partial/imageProfile', array('cover' => 'getImg?img='.Auth::user()->avatar.'&w=200', 'id' => 'myAvatar', 'border' => '#fff', 'borderSize' => '1px'))
-            </div>
-
-             <div class=' hidden-xs col-xs-2 col-sm-2 padding-top text-left dropdown'>
-
-                <div class='col-xs-12 not-padding menu dropdown-toggle flex-center ' data-toggle="dropdown">
-
-                    <span class='not-padding not-margin paragraph4  text-white'>{{Auth::user()->first_name}}</span>
-                    <i class='iconmenu menu fa fa-angle-down'></i>
-
-                </div>
-
-                <ul class="dropdown-menu">
-                    <li>
-                        <a href="{{ url('profile') }}">{{ trans('nav.profile') }}</a>
-                    </li>
-                    <li>
-                        <a href="{{ url('/validateLogout') }}" class="link1">{{ trans('nav.logout') }}</a>
-                    </li>
-                </ul>
-
-            </div>
-
-            <ul class="menuMobile visible-xs col-xs-2 col-xs-offset-3 not-padding">
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                       aria-expanded="false"><i class="fa fa-bars"></i></a>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a href="{{ url('profile') }}"><i
-                                        class="material-icons item-icon">account_circle</i> {{ trans('nav.profile') }}</a>
-                        </li>
-                        <li>
-                            <a href="/how">
-                                <i class="material-icons item-icon">info</i> {{ trans('nav.howitworks') }}
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ url('/validateLogout') }}"><i
-                                        class="material-icons item-icon">exit_to_app</i> {{ trans('nav.logout') }}</a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-
-        </div>
-    </div>
-
-</nav>
+    @include('nav', array('type' => 2))
 
     <div class="container">
        <div class="space"></div>
