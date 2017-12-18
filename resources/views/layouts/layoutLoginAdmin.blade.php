70f4357cbcd6ba7d7e18615e757b4982ae1cba9e
ccdaza <ccdaza2@gmail.com>
2017-12-18 11:58:44 -0500

    nuevo estilo login admin

--- resources/views/layouts/layoutLoginAdmin.blade.php
+++ resources/views/layouts/layoutLoginAdmin.blade.php
@@ -9,21 +9,12 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Banco de Tiempo Bogotá</title>    
-    <!-- Material Design fonts -->
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700">
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/icon?family=Material+Icons">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap -->
-    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-    <!-- Bootstrap Material Design -->
-    <link rel="stylesheet" type="text/css" href="css/site.css">
-    <link rel="stylesheet" type="text/css" href="css/ripples.min.css">
-    <!-- Custom styles -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" >
+  
     <link href="css/styleAdmin.css" rel="stylesheet">
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/vue.js') }}"></script>   
+  
     <script>
         window.Laravel = <?php echo json_encode([
             'csrfToken' => csrf_token(),
@@ -31,29 +22,12 @@
     </script>
 </head>
 <body>
-    <div id="app">
-      <!--Navbar-->
-        <nav class="navbar navbar-default navbar-static-top">
-          <div class="container">
-            <div class="navbar-header">          
-              <!-- Branding Image -->
-              <a class="navbar-brand titleNav" href="{{ url('/admin') }}">
-                Banco de Tiempo de Bogotá
-              </a>
-            </div>
-          </div>
-        </nav>
-      <!--/.Navbar-->
-
+    <div id="loginAdmin">
+     
         @yield('content')
     </div>
-
-    <!-- Scripts -->    
-    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>  
-    <script src="js/material.min.js"></script>   
-    <script src="js/ripples.min.js"></script>   
-    <script src="{{ asset('/js/script.js') }}"></script>  
-    <script src="{{ asset('/js/components/modal.js') }}"></script>   
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" ></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" ></script>
 </body>
 </html>
