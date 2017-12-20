0de0c59468530d9b7f8404d79b56c85901a28272
ccdaza <ccdaza2@gmail.com>
2017-12-20 17:23:06 -0500

    notificaciones propuestas 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,9 +81,6 @@
         <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCPGPS5eThFsyJBtOl7RYlaFEp4HLRKKWA&libraries=places"></script>
     @endif
         <script src="{{ asset('js/sitio.js') }}"></script>
-        <script src="https://www.gstatic.com/firebasejs/4.2.0/firebase-app.js"></script>
-        <script src="https://www.gstatic.com/firebasejs/4.2.0/firebase-auth.js"></script>
-        <script src="https://www.gstatic.com/firebasejs/4.2.0/firebase-database.js"></script>
-        <script src="https://www.gstatic.com/firebasejs/4.2.0/firebase-messaging.js"></script>
-        <script src="{{ asset('js/notifications.js') }}"></script>
+        
+        @yield('script')
 </html>
