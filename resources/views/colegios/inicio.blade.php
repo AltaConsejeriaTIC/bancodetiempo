97d58d5212df503d89c706bcba0f3c054291fbd1
dfnavas <danielnavasm@gmail.com>
2018-01-24 17:50:17 -0500

    registro de colegios modificado, faltan margenes  en los campos de texto

--- resources/views/colegios/inicio.blade.php
+++ resources/views/colegios/inicio.blade.php
@@ -6,13 +6,13 @@
     <div class="container pt-5">
         <div class="row justify-content-center pt-5">
             <div class="col-12 col-md-8 bg-white-opacity rounded p-4">
-                <h6 class="text-white font-weight-bold ml-5">Abre una cuenta</h6>
+                <h5 class="text-white font-weight-bold ml-5">Abre una cuenta</h5>
                 <div class="row">
                     <div class="col-md text-center">
-                        <a href="/registro-estudiante" class="btn btn-dark bg-register rounded text-lg font-weight-bold px-4 my-2">¡Soy estudiante!</a>
+                        <a href="/registro-estudiante" class="btn btn-success bg-register-btn rounded text-lg font-weight-bold px-4 my-2">¡Soy estudiante!</a>
                     </div>
                     <div class="col-md text-center">
-                        <a href="/registro-admin" class="btn btn-dark bg-register rounded text-lg font-weight-bold px-4 my-2">¡Soy administrador!</a>
+                        <a href="/registro-admin" class="btn btn-success bg-register-btn rounded text-lg font-weight-bold px-4 my-2">¡Soy administrador!</a>
                     </div>
                 </div>
             </div>
--- resources/views/colegios/nav.blade.php
+++ resources/views/colegios/nav.blade.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-cambalachea pl-lg-5 pt-3 pb-4">
+<nav class="navbar navbar-expand-lg navbar-dark bg-cambalachea pl-lg-5 pt-3 pb-3">
 	<a class="navbar-brand" href="/">
-		<img src="/images/logo2.png" alt="Bogotá cambalachea">
+		<img src="/images/logocolegiosnav2.png" alt="Bogotá cambalachea">
 	</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
@@ -9,7 +9,7 @@
 		<div class="navbar-nav mr-auto"></div>
 		<form action="/login" method="post" class="row form-inline my-2 my-lg-0">
 			{{ csrf_field() }}
-			<div class="col-md">
+			<div class="col-md mb-md-4">
 				<label for="" class="text-white text-sm d-block">Email</label>
 				<input type="text" name="email" class="form-control rounded text-sm">
 			</div>
@@ -17,10 +17,11 @@
 				<label for="" class="text-white text-sm d-block">Contraseña</label>
 				<input type="password" name="password" class="form-control rounded text-sm">
 				<a class="text-white text-xs position-absolute">¿Olvidaste la contraseña?</a>
+				<!-- <a class="text-white text-xs d-md-block">¿Olvidaste la contraseña?</a> -->
 			</div>
-			<div class="col-md">
-				<p class="text-sm mb-0">&nbsp;</p>
+			<div class="col-md mt-md-3">
 				<button type="submit" class="btn bg-login-btn text-white rounded text-sm">Iniciar sesión</button>
+				<p class="text-sm mb-0">&nbsp;</p>
 			</div>
 		</form>
 	</div>
